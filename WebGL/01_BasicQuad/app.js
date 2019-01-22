6ae6c58b25df36470b10bd52233a66ae1ac8acb6
scahp <scahp@naver.com>
2019-01-22 13:57:44 +0900

    ObjectArray
    ObjectArray Added

--- WebGL/01_BasicQuad/app.js
+++ WebGL/01_BasicQuad/app.js
@@ -1,5 +1,64 @@
 var jWebGL;
 
+var createStaticObject = function(gl, vertices, vsCode, fsCode, attribParameters)
+{
+    var vbo = gl.createBuffer();
+    var program = CreateProgram(gl, 'shaders/vs.glsl', 'shaders/fs.glsl');
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var attribs = [];
+    for(var i=0;i<attribParameters.length;++i)
+    {
+        var attr = attribParameters[i];
+
+        var loc = gl.getAttribLocation(program, attr.name);
+        attribs[i] = { name:attr.name
+            , loc:loc
+            , count:attr.count
+            , type:attr.type
+            , normalized:attr.normalized
+            , stride:attr.stride
+            , offset:attr.offset };
+    }
+
+    return {vbo:vbo, program:program, attribs:attribs};
+}
+
+var createAttribParameter = function(name, count, type, normalized, stride, offset)
+{
+    return { name:name, count:count, type:type, normalized:normalized, stride:stride, offset:offset };
+}
+
+var bindAttribPointer = function(gl, StaticObject)
+{
+    gl.bindBuffer(gl.ARRAY_BUFFER, StaticObject.vbo);
+    for(var i=0;i<StaticObject.attribs.length;++i)
+    {
+        var attrib = StaticObject.attribs[i];
+        gl.vertexAttribPointer(attrib.loc,
+            attrib.count,
+            attrib.type,
+            attrib.normalized,
+            attrib.stride,
+            attrib.offset);
+
+        gl.enableVertexAttribArray(attrib.loc);
+    }
+}
+
+var drawStaticObject = function(gl, StaticObject)
+{
+    bindAttribPointer(gl, StaticObject);
+
+    gl.useProgram(StaticObject.program);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);    
+}
+
+var StaticObjectArray = [];
+
 var Init = function()
 {
     console.log('init function start');
@@ -34,19 +93,39 @@ jWebGL.prototype.Init = function()
 
     this.OnResizeWindow();
     
-    this.program = CreateProgram(gl, 'shaders/vs.glsl', 'shaders/fs.glsl');
+    // ObjectA
+    {
+        var vertices = [
+            // VertexID     Color           Offset          Scale
+            2,              0, 0, 1,        0.3, 0.3,       0.6,
+            0,              1, 0, 0,        0.3, 0.3,       0.6,
+            3,              1, 1, 1,        0.3, 0.3,       0.6,
+            1,              0, 1, 0,        0.3, 0.3,       0.6,
+        ];
 
-    var vertices = [
-        0, 1, 0, 0,
-        1, 0, 1, 0,
-        2, 0, 0, 1,
-        3, 1, 1, 1
-    ];
+        var attrib0 = createAttribParameter('VertexID', 1, gl.FLOAT, false, Float32Array.BYTES_PER_ELEMENT * 7, 0);
+        var attrib1 = createAttribParameter('Color', 3, gl.FLOAT, false, Float32Array.BYTES_PER_ELEMENT * 7, Float32Array.BYTES_PER_ELEMENT * 1);
+        var attrib2 = createAttribParameter('Offset', 2, gl.FLOAT, false, Float32Array.BYTES_PER_ELEMENT * 7, Float32Array.BYTES_PER_ELEMENT * 4);
+        var attrib3 = createAttribParameter('Scale', 1, gl.FLOAT, false, Float32Array.BYTES_PER_ELEMENT * 7, Float32Array.BYTES_PER_ELEMENT * 6);
+        StaticObjectArray.push(createStaticObject(gl, vertices, 'shaders/vs.glsl', 'shaders/fs.glsl', [attrib0, attrib1, attrib2, attrib3]));
+    }
 
-    this.vbo = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    // ObjectB
+    {
+        var vertices = [
+            // VertexID     Color           Offset          Scale
+            0,              1, 0, 0,        -0.3, -0.3,     0.3,
+            1,              0, 1, 0,        -0.3, -0.3,     0.3,
+            2,              0, 0, 1,        -0.3, -0.3,     0.3,
+            3,              1, 1, 1,        -0.3, -0.3,     0.3,
+        ];
+
+        var attrib0 = createAttribParameter('VertexID', 1, gl.FLOAT, false, Float32Array.BYTES_PER_ELEMENT * 7, 0);
+        var attrib1 = createAttribParameter('Color', 3, gl.FLOAT, false, Float32Array.BYTES_PER_ELEMENT * 7, Float32Array.BYTES_PER_ELEMENT * 1);
+        var attrib2 = createAttribParameter('Offset', 2, gl.FLOAT, false, Float32Array.BYTES_PER_ELEMENT * 7, Float32Array.BYTES_PER_ELEMENT * 4);
+        var attrib3 = createAttribParameter('Scale', 1, gl.FLOAT, false, Float32Array.BYTES_PER_ELEMENT * 7, Float32Array.BYTES_PER_ELEMENT * 6);
+        StaticObjectArray.push(createStaticObject(gl, vertices, 'shaders/vs.glsl', 'shaders/fs.glsl', [attrib0, attrib1, attrib2, attrib3]));
+    }
 
     var t = this;
 
@@ -65,29 +144,8 @@ jWebGL.prototype.Render = function()
     gl.clearColor(0.5, 0.5, 0.5, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    var vertexIDAttribLoc = gl.getAttribLocation(this.program, 'VertexID');
-    var colorAttribLoc = gl.getAttribLocation(this.program, 'Color');
-
-    gl.vertexAttribPointer(vertexIDAttribLoc,
-        1,
-        gl.FLOAT,
-        gl.FALSE,
-        Float32Array.BYTES_PER_ELEMENT * 4,
-        0);
-    
-    gl.vertexAttribPointer(colorAttribLoc,
-        3,
-        gl.FLOAT,
-        gl.FALSE,
-        Float32Array.BYTES_PER_ELEMENT * 4,
-        Float32Array.BYTES_PER_ELEMENT * 1);
-    
-    gl.enableVertexAttribArray(vertexIDAttribLoc);
-    gl.enableVertexAttribArray(colorAttribLoc);
-
-    gl.useProgram(this.program);
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    for(var i = 0;i<StaticObjectArray.length;++i)
+        drawStaticObject(gl, StaticObjectArray[i]);
 }
 
 jWebGL.prototype.OnResizeWindow = function()
--- WebGL/01_BasicQuad/shaders/vs.glsl
+++ WebGL/01_BasicQuad/shaders/vs.glsl
@@ -2,24 +2,27 @@ precision mediump float;
 
 attribute float VertexID;
 attribute vec3 Color;
+attribute vec2 Offset;
+attribute float Scale;
 
 varying vec3 Color_;
 
 void main()
 {
     vec2 pos[4];
-    pos[0].x = -0.5;    pos[0].y = 0.5;
-    pos[1].x = 0.5;     pos[1].y = 0.5;
-    pos[2].x = -0.5;    pos[2].y = -0.5;
-    pos[3].x = 0.5;     pos[3].y = -0.5;
+    float size = 0.5 * Scale;
+    pos[0].x = -size;    pos[0].y = size;
+    pos[1].x = size;     pos[1].y = size;
+    pos[2].x = -size;    pos[2].y = -size;
+    pos[3].x = size;     pos[3].y = -size;
 
     Color_ = Color;
     if (0.0 == VertexID)
-        gl_Position = vec4(pos[0], 0.0, 1.0);
+        gl_Position = vec4(pos[0] + Offset, 0.0, 1.0);
     else if (1.0 == VertexID)
-        gl_Position = vec4(pos[1], 0.0, 1.0);
+        gl_Position = vec4(pos[1] + Offset, 0.0, 1.0);
     else if (2.0 == VertexID)
-        gl_Position = vec4(pos[2], 0.0, 1.0);
+        gl_Position = vec4(pos[2] + Offset, 0.0, 1.0);
     else if (3.0 == VertexID)
-        gl_Position = vec4(pos[3], 0.0, 1.0);
+        gl_Position = vec4(pos[3] + Offset, 0.0, 1.0);
 }
